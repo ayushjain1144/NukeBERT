d2b02c03129ef633ce4f5705654b3a20726d8ddd
ayush jain <ayushjain1144@gmail.com>
2019-06-25 09:27:49 +0530

    added functionalities

--- clean.py
+++ clean.py
@@ -1,4 +1,7 @@
 import re
+from gensim.parsing.preprocessing import strip_multiple_whitespaces, strip_short , strip_non_alphanum
+from gensim.summarization.textcleaner import undo_replacement
+
 
 
 
@@ -9,3 +12,12 @@ def remove_citations(text):
 	text = re.sub(r'et al. ', '', text)
 
 	return text	
+
+def clean_line(line):
+	line = strip_non_alphanum(line)
+	line = strip_multiple_whitespaces(line)
+	line = strip_short(line)
+	line = undo_replacement(line)
+	line = line + '.'
+
+	return line	
